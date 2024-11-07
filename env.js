fe5e2b32701547446af1e0aabc177d30c08c7fa4
sean-seibel <seibel.s@northeastern.edu>
2024-11-07 15:25:02 -0500

    new correct root

--- env.js
+++ env.js
@@ -1,6 +1,6 @@
 const _DOMAIN = "cw-2-3c00dc28a5eb.herokuapp.com"
 const _PORT = "443"
-const _ROOT = "cw-client"
+const _ROOT = "/cw-client"
 const _GAMEPAGE = `${_ROOT}/game/index.html`
 const _HOMEPAGE = `${_ROOT}/index.html`
 const _URL = `https://${_DOMAIN}`
